f73c2956079f2a58606322847b822e47237be361
minhtriizkooooo-sys <minhtriizkooooo@gmail.com>
2025-11-05 04:46:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,5 +149,6 @@ def logout():
 
 # --- Run app ---
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
